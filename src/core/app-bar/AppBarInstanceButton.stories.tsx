8862319f44d1ca48ada6ff4f23a11ccac6de3bf1
Brad Taniguchi <bradtaniguchi@gmail.com>
2023-10-08 19:34:46 -0700

    chore: update appbar instance

---
+++ src/core/app-bar/AppBarInstanceButton.stories.tsx
@@ -0,0 +1,21 @@
+import { Meta } from "@storybook/react";
+import { AppBarInstanceButton } from "./AppBarInstanceButton";
+import { HELLO_WORLD_APP } from "../../apps/hello-world-def";
+import { getId } from "../../utils/get-id";
+
+const meta: Meta<typeof AppBarInstanceButton> = {
+  component: AppBarInstanceButton,
+};
+
+export default meta;
+
+export const Default = () => (
+  <AppBarInstanceButton
+    appInstance={{
+      ...HELLO_WORLD_APP,
+      instanceId: getId(),
+      viewState: "normal",
+    }}
+    onClick={() => {}}
+  />
+);
