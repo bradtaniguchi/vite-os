78834b834dfa6753411ccd52596fd6f4ae3d8901
bradtaniguchi <bradtaniguchi@gmail.com>
2023-10-09 02:41:16 +0000

    Deploying to gh-pages from @ bradtaniguchi/vite-os@8862319f44d1ca48ada6ff4f23a11ccac6de3bf1 🚀

---
+++ storybook/assets/AppBarInstanceButton.stories-f16cec07.js
@@ -0,0 +1,6 @@
+import{j as r,H as s}from"./hello-world-def-ec278bc5.js";import{A as o}from"./AppBarInstanceButton-02362e6c.js";import{g as p}from"./get-id-2999bd34.js";import"./index-c013ead5.js";import"./_commonjsHelpers-725317a4.js";import"./index-169ee69c.js";const I={component:o},t=()=>r.jsx(o,{appInstance:{...s,instanceId:p(),viewState:"normal"},onClick:()=>{}});var a,e,n;t.parameters={...t.parameters,docs:{...(a=t.parameters)==null?void 0:a.docs,source:{originalSource:`() => <AppBarInstanceButton appInstance={{
+  ...HELLO_WORLD_APP,
+  instanceId: getId(),
+  viewState: "normal"
+}} onClick={() => {}} />`,...(n=(e=t.parameters)==null?void 0:e.docs)==null?void 0:n.source}}};const f=["Default"];export{t as Default,f as __namedExportsOrder,I as default};
+//# sourceMappingURL=AppBarInstanceButton.stories-f16cec07.js.map
